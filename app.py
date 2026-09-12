fb20feb72c3dd93973d8c9ffb0e31fe9f4f10ae2
Carlos Henrique <carloshmf.09@gmail.com>
2026-09-11 22:44:05 -0300

    feat: implement progressive discount based on total value

---
+++ app.py
@@ -0,0 +1,23 @@
+valor = float(input("Bem vindo! Digite o valor da compra: "))
+porcentagem = 0
+desconto = 0
+valor_descontado = 0
+if valor >= 300:
+    porcentagem = 15
+    valor_descontado = valor * 0.15
+    desconto = valor - valor_descontado
+else:
+    if valor >= 200:
+        porcentagem = 10
+        valor_descontado = valor * 0.10
+        desconto = valor - valor_descontado
+    else:
+        if valor < 200:
+            porcentagem = 5
+            valor_descontado = valor * 0.05
+            desconto = valor - valor_descontado
+print(f"Você recebeu {porcentagem}% de desconto, o valor a ser pago será de R$ {desconto:.2f}")
+print(f"O desconto foi de R$ {valor_descontado:.2f}")
+print(f"Obrigado por nos visitar, até a próxima!")            
+
+        

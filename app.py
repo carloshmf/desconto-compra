a9934b458b3f88d4ec1a52115463255d5f2babca
Carlos Henrique <carloshmf.09@gmail.com>
2026-09-17 17:15:35 -0300

    fix: remove unnecessary "x" from file

--- app.py
+++ app.py
@@ -19,6 +19,6 @@ else:
 print(f"Você recebeu {porcentagem}% de desconto, o valor a ser pago será de R$ {desconto:.2f}")      # saída de dados
 print(f"O desconto foi de R$ {valor_descontado:.2f}")
 print(f"Obrigado por nos visitar, até a próxima!") 
-x           
+           
 
         
